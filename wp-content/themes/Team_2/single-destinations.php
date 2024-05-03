b766b65567b320aee4b6a2c5dbc65360b1cbea26
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-05-03 18:05:47 +0200

    first commit

--- wp-content/themes/Team_2/single-destinations.php
+++ wp-content/themes/Team_2/single-destinations.php
@@ -1,5 +1,20 @@
-<?php get_header(); ?>
-<div class="card-style mt-5">
-    <button> Sono la pagina destinations</button>
+<?php
+
+get_header();
+the_post(); ?>
+
+<div class="containe my-4 mx-3">
+    <div class="row">
+        <div class="col-12 col-md-9">
+            <div class="card">
+            </div>
+        </div>
+        <div class="col-12 col-md-3">
+            <div class="card"> <?php get_sidebar() ?>
+            </div>
+        </div>
+    </div>
 </div>
-<?php get_footer(); ?>
+
+<?php
+get_footer();
