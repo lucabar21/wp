cfbae6ff6f2312cc8066c3136a9aed5d7656ddc4
SimoCrisce <126293808+SimoCrisce@users.noreply.github.com>
2024-05-06 10:08:18 +0200

    header update

--- wp-content/themes/Team_2/header.php
+++ wp-content/themes/Team_2/header.php
@@ -28,7 +28,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-3 ms-auto gap-3 mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" href="<?php site_url("/")?>">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="<?php site_url("/about") ?>">About</a>
