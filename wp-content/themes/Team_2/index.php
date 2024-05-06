025d9545a09dd677ed36fe6fbc3e0edb92c97330
lucabar21 <luca_barbuto@yahoo.it>
2024-05-06 10:42:40 +0200

    home-page static structure commit

--- wp-content/themes/Team_2/index.php
+++ wp-content/themes/Team_2/index.php
@@ -1,6 +1,3 @@
 <?php get_header(); ?>
-<div class="card-style mt-5">ciao </br>
-    ciao
-    <button> Clicca</button>
-</div>
+
 <?php get_footer(); ?>
--- wp-content/themes/Team_2/page-home.php
+++ wp-content/themes/Team_2/page-home.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+<!-- HERO SECTION -->
 <div class="hp-section my-4 mx-3">
     <img class="hero-img" src="http://placedog.net/455" alt="hero">
     <div class="hero-text">
@@ -8,10 +9,65 @@
         <button>Scopri di più</button>
     </div>
 </div>
+<!-- PROMO -->
+<div class="hp-bot-section-3 my-4 mx-3">
+    <div id="promo-img"><img src="wp-content\uploads\2024\05\promo.png" alt="promo"></div>
+    <div id="promo-btn">
+        <h5>APPROFITTA DEI NOSTRI IMPERDIBILI SCONTI VIAGGIO!</h5>
+        <div>COUPON 50%</div>
+    </div>
+</div>
+
+<!-- LATEST POST SECTION -->
 <div class="hp-section-2 my-4 mx-3">
     <h5>Dai un'occhiata alle destinazioni che abbiamo proposto le settimane precedenti.</h5>
-    <div class="d-flex justify-content-center">
-        <div class="hp-card"></div>
+    <div class="row justify-content-center past">
+        <div class="col-auto hp-card">
+            <img src="http://placedog.net/200" alt="card">
+            <div class="hero-text">
+                <h2>TITOLO DESTINAZIONE</h2>
+                <button>Scopri di più</button>
+            </div>
+        </div>
+        <div class="hp-card col-auto">
+            <img src="http://placedog.net/201" alt="card">
+            <div class="hero-text">
+                <h2>TITOLO DESTINAZIONE</h2>
+                <button>Scopri di più</button>
+            </div>
+        </div>
+        <div class="hp-card col-auto">
+            <img src="http://placedog.net/202" alt="card">
+            <div class="hero-text">
+                <h2>TITOLO DESTINAZIONE</h2>
+                <button>Scopri di più</button>
+            </div>
+        </div>
+        <div class="hp-card col-auto">
+            <img src="http://placedog.net/203" alt="card">
+            <div class="hero-text">
+                <h2>TITOLO DESTINAZIONE</h2>
+                <button>Scopri di più</button>
+            </div>
+        </div>
     </div>
+</div>
+<!-- CALL TO ACTION FORM -->
+<div class="hp-top-section-3 my-4 mx-3">
+    <h5 class="my-3">Consigliaci una destinazione che vorresti che presentassimo nelle prossime settimane!</h5>
+    <form>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Inserisci il tuo nome</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Inserisci il tuo indirizzo email</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Dacci un consiglio...</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+    </form>
 </div>
 <?php get_footer(); ?>
