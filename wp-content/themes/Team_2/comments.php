cc73dd583b22de1585823bdd6029633bf28f155b
SimoCrisce <126293808+SimoCrisce@users.noreply.github.com>
2024-05-08 12:53:11 +0200

    comment section styling

--- wp-content/themes/Team_2/comments.php
+++ wp-content/themes/Team_2/comments.php
@@ -1,6 +1,30 @@
 <?php
+$commenter = wp_get_current_commenter();
+$req = get_option( 'require_name_email' );
+$aria_req = ( $req ? " aria-required='true'" : '' );
+$args = array(
+	'comment_field' => '<div class="form-floating mb-3"><label for="floatingTextarea">' . _x( 'Comment', 'noun' ) . '</label><textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" name="comment" cols="45" rows="8" aria-required="true"></textarea></div>',
+    'fields' => apply_filters( 'comment_form_default_fields', array(
+
+    'author' =>
+      '<div class="input-group mb-3">' .
+      '<input id="author" name="author" type="text" class="form-control" placeholder="Nome" value="' . esc_attr( $commenter['comment_author'] ) .
+      '" size="30"' . $aria_req . ' /></div>',
+
+    'email' =>
+      '<div class="input-group mb-3">' .
+      '<input id="email" name="email" type="text" class="form-control" placeholder="Email" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+      '" size="30"' . $aria_req . ' /></div>',
+
+    'url' =>
+      '<div class="input-group mb-3">' .
+      '<input id="url" name="url" type="text" class="form-control" placeholder="Sito Web" value="' . esc_attr( $commenter['comment_author_url'] ) .
+      '" size="30" /></div>'
+    ))
+
+);
 if (comments_open() || get_comments_number()) :
-    comment_form();
+    comment_form($args);
     if (have_comments()) :
 ?>
         <ol class="comment-list">
